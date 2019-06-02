080d9c35bb956a9af8526a4ec1496b54311490c9
ibrahim <ibrahimibrahim408@gmail.com>
2019-06-01 23:39:48 -0700

    removed navigation

--- housingApp/housingApp/navigation/AppNavigator.js
+++ housingApp/housingApp/navigation/AppNavigator.js
@@ -1,11 +1,9 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation';
-import Home from '../src/components/Home';
 import Discover from '../src/components/Discover';
 
 const AppNavigator = createStackNavigator(
  {
     Discover: { screen: Discover },
-    Home: { screen: Home},
  },
  {
 	initialRouteName: "Discover",
